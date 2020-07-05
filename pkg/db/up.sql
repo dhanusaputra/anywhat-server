7c42f5bc4298769a9f1290998fa93207be4741d6
dhanu <andreasdhanu@gmail.com>
2020-07-05 12:05:14 +0700

    fix db

--- pkg/db/up.sql
+++ pkg/db/up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS anywhat (
-  id CHAR(27),
-  name VARCHAR(24) NOT NULL
+  id CHAR(27) UNIQUE,
+  name VARCHAR(24) NOT NULL,
   description VARCHAR(1024) DEFAULT NULL,
   createdAt timestamp NULL DEFAULT NULL,
   updatedAt timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY 'id_unique' (id)
+  PRIMARY KEY (id)
 );
