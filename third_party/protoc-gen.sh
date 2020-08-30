4120b7d49bc8731e0e328b9fc26b98e24011b3cf
dhanu <andreasdhanu@gmail.com>
2020-08-30 09:46:25 +0700

    add make file

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,2 +1,3 @@
 protoc --proto_path=api/pb --go_out=plugins=grpc:api/pb anywhat.proto
 protoc --proto_path=api/pb --swagger_out=logtostderr=true:third_party/swagger-ui anywhat.proto
+protoc --proto_path=api/pb --swagger_out=logtostderr=true:api/swagger anywhat.proto
