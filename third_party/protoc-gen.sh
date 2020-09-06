2351830ff818967cc94fe864e277a60bb073b176
dhanu <andreasdhanu@gmail.com>
2020-09-06 19:55:18 +0700

    fin user proto

--- third_party/protoc-gen.sh
+++ third_party/protoc-gen.sh
@@ -1,3 +1,6 @@
 protoc --proto_path=api/pb --go_out=plugins=grpc:api/pb anywhat.proto
+protoc --proto_path=api/pb --go_out=plugins=grpc:api/pb user.proto
 protoc --proto_path=api/pb --swagger_out=logtostderr=true:third_party/swagger-ui anywhat.proto
+protoc --proto_path=api/pb --swagger_out=logtostderr=true:third_party/swagger-ui user.proto
 protoc --proto_path=api/pb --swagger_out=logtostderr=true:api/swagger anywhat.proto
+protoc --proto_path=api/pb --swagger_out=logtostderr=true:api/swagger user.proto
