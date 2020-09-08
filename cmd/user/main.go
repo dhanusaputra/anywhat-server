f6caa934f64c12e287345a480cae99fba8057aff
dhanu <andreasdhanu@gmail.com>
2020-09-08 08:43:25 +0700

    setup docker

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"fmt"
 
-	"github.com/dhanusaputra/anywhat-server/pkg/cmd"
+	"github.com/dhanusaputra/anywhat-server/pkg/cmd/anywhat"
 	"github.com/dhanusaputra/anywhat-server/pkg/service"
 	"github.com/dhanusaputra/anywhat-server/util/envutil"
 	_ "github.com/joho/godotenv/autoload"
@@ -21,7 +21,7 @@ var (
 )
 
 func main() {
-	var cfg cmd.Config
+	var cfg anywhat.Config
 	flag.StringVar(&cfg.GRPCPort, "grpc-port", "9090", "gRPC port to bind")
 	flag.IntVar(&cfg.LogLevel, "log-level", -1, "Global log level")
 	flag.StringVar(&cfg.LogTimeFormat, "log-time-format", "2006-01-02T15:04:05.999999999Z07:00", "Print time format for logger e.g. 006-01-02T15:04:05Z07:00")
@@ -37,7 +37,7 @@ func main() {
 	defer db.Close()
 
 	s := service.NewAnywhatService(db)
-	if err := cmd.ListenGRPC(s, cfg); err != nil {
+	if err := anywhat.ListenGRPC(s, cfg); err != nil {
 		panic(err)
 	}
 }
