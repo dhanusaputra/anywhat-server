26409d2ea1fc85111cfc70bcc31942ba7080eed8
dhanu <andreasdhanu@gmail.com>
2020-09-08 08:39:27 +0700

    add package user

---
+++ pkg/cmd/anywhat/server.go
@@ -0,0 +1,96 @@
+package anywhat
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"go.uber.org/zap"
+
+	"github.com/dhanusaputra/anywhat-server/api/pb"
+	"github.com/dhanusaputra/anywhat-server/pkg/logger"
+	"github.com/dhanusaputra/anywhat-server/pkg/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type grpcServer struct {
+	anywhat service.Anywhat
+}
+
+// Config is configuration for Server
+type Config struct {
+	// gRPC server start parameters section
+	// gRPC is TCP port to listen by gRPC server
+	GRPCPort string
+
+	// Log parameters section
+	// LogLevel is global log level: Debug(-1), Info(0), Warn(1), Error(2), DPanic(3), Panic(4), Fatal(5)
+	LogLevel int
+	// LogTimeFormat is print time format for logger e.g. 2006-01-02T15:04:05Z07:00
+	LogTimeFormat string
+}
+
+// ListenGRPC ...
+func ListenGRPC(s service.Anywhat, cfg Config) error {
+	lis, err := net.Listen("tcp", ":"+cfg.GRPCPort)
+	if err != nil {
+		return err
+	}
+	// initialize logger
+	if err := logger.Init(cfg.LogLevel, cfg.LogTimeFormat); err != nil {
+		return fmt.Errorf("failed to initialize logger: %v", err)
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAnywhatServer(serv, &grpcServer{s})
+	reflection.Register(serv)
+	// start gRPC server
+	logger.Log.Info("starting gRPC server...", zap.String("port", cfg.GRPCPort))
+	return serv.Serve(lis)
+}
+
+// GetAnything ...
+func (s *grpcServer) GetAnything(ctx context.Context, req *pb.GetAnythingRequest) (*pb.GetAnythingResponse, error) {
+	a, err := s.anywhat.Get(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.GetAnythingResponse{Anything: a}, nil
+}
+
+// ListAnything ...
+func (s *grpcServer) ListAnything(ctx context.Context, _ *emptypb.Empty) (*pb.ListAnythingResponse, error) {
+	as, err := s.anywhat.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ListAnythingResponse{Anythings: as}, nil
+}
+
+// CreateAnything ...
+func (s *grpcServer) CreateAnything(ctx context.Context, req *pb.CreateAnythingRequest) (*pb.CreateAnythingResponse, error) {
+	id, err := s.anywhat.Create(ctx, req.Anything)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.CreateAnythingResponse{Id: id}, nil
+}
+
+// UpdateAnything ...
+func (s *grpcServer) UpdateAnything(ctx context.Context, req *pb.UpdateAnythingRequest) (*pb.UpdateAnythingResponse, error) {
+	success, err := s.anywhat.Update(ctx, req.Anything)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.UpdateAnythingResponse{Updated: success}, nil
+}
+
+// DeleteAnything ...
+func (s *grpcServer) DeleteAnything(ctx context.Context, req *pb.DeleteAnythingRequest) (*pb.DeleteAnythingResponse, error) {
+	success, err := s.anywhat.Delete(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.DeleteAnythingResponse{Deleted: success}, nil
+}
