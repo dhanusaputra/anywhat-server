933e3dcac37c2891e01c5ea9f0914228c4e1c74c
dhanu <andreasdhanu@gmail.com>
2020-10-02 06:57:47 +0700

    impl

--- pkg/graph/schema.resolvers.go
+++ pkg/graph/schema.resolvers.go
@@ -5,19 +5,34 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/dhanusaputra/anywhat-server/api/pb"
 	"github.com/dhanusaputra/anywhat-server/pkg/graph/generated"
 	"github.com/dhanusaputra/anywhat-server/pkg/graph/model"
+	"github.com/golang/protobuf/ptypes/empty"
 )
 
 func (r *mutationResolver) CreateAnything(ctx context.Context, input *model.AnythingInput) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	res, err := r.anywhatClient.CreateAnything(ctx, &pb.CreateAnythingRequest{Anything: &pb.Anything{
+		Name:        input.Name,
+		Description: input.Description,
+	}})
+	if err != nil {
+		return "", err
+	}
+	return res.Id, nil
 }
 
 func (r *mutationResolver) UpdateAnything(ctx context.Context, id string, input *model.AnythingInput) (bool, error) {
-	panic(fmt.Errorf("not implemented"))
+	res, err := r.anywhatClient.UpdateAnything(ctx, &pb.UpdateAnythingRequest{Anything: &pb.Anything{
+		Id:          id,
+		Name:        input.Name,
+		Description: input.Description,
+	}})
+	if err != nil {
+		return false, err
+	}
+	return res.Updated, nil
 }
 
 func (r *queryResolver) GetAnything(ctx context.Context, id string) (*model.Anything, error) {
@@ -35,15 +50,41 @@ func (r *queryResolver) GetAnything(ctx context.Context, id string) (*model.Anyt
 }
 
 func (r *queryResolver) ListAnything(ctx context.Context) ([]*model.Anything, error) {
-	panic(fmt.Errorf("not implemented"))
+	res, err := r.anywhatClient.ListAnything(ctx, new(empty.Empty))
+	if err != nil {
+		return nil, err
+	}
+	a := res.Anythings
+	m := make([]*model.Anything, len(a))
+	for _, aa := range a {
+		m = append(m, &model.Anything{
+			ID:          aa.Id,
+			Name:        aa.Name,
+			Description: aa.Description,
+			CreatedAt:   aa.CreatedAt.AsTime(),
+			UpdatedAt:   aa.UpdatedAt.AsTime(),
+		})
+	}
+	return m, nil
 }
 
 func (r *queryResolver) Login(ctx context.Context, username string, password string) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	res, err := r.userClient.Login(ctx, &pb.LoginRequest{Username: username, Password: password})
+	if err != nil {
+		return "", err
+	}
+	return res.Token, nil
 }
 
 func (r *queryResolver) Me(ctx context.Context) (*model.User, error) {
-	panic(fmt.Errorf("not implemented"))
+	res, err := r.userClient.Me(ctx, new(empty.Empty))
+	if err != nil {
+		return nil, err
+	}
+	return &model.User{
+		ID:       res.User.Id,
+		Username: res.User.Username,
+	}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
