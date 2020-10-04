ce340edf814d3452fce8e9581f7488ac6676a0ba
dhanu <andreasdhanu@gmail.com>
2020-10-04 08:03:05 +0700

    remove log

--- pkg/graph/middleware/auth.go
+++ pkg/graph/middleware/auth.go
@@ -3,10 +3,8 @@ package middleware
 import (
 	"net/http"
 
-	"github.com/dhanusaputra/anywhat-server/pkg/logger"
 	"github.com/dhanusaputra/somewhat-server/util/authutil"
 	"github.com/dhanusaputra/somewhat-server/util/envutil"
-	"go.uber.org/zap"
 )
 
 const (
@@ -21,7 +19,6 @@ var (
 func AddAuth() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger.Log.Info("test", zap.Any("r", r.TransferEncoding))
 			if !authEnable {
 				next.ServeHTTP(w, r)
 				return
